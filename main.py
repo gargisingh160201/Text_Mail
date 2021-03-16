712a643c30be45fb66887dee92c79b65b9a53f78
Deepam Gupta <deepam.deepam.gupta@gmail.com>
2021-03-16 18:32:30 +0530

    done for gmail, outlook, thunderbird, yahoo, mailspring

--- main.py
+++ main.py
@@ -11,16 +11,21 @@ def mail_thread_to_text(mail_txt: str):
     if not is_html:
         return mail_txt
 
-    first_div = soup.find('div')
+    # check for thunder bird
+
+    first_div = soup.find(['div', 'p']) # p for thunderbird
     div_siblings = [first_div] + (first_div.find_next_siblings('div'))
 
     pure_text = ''
 
-    for div in div_siblings:
-        if not div.has_attr('class'):
-            pure_text += '{}\n'.format(div.text)
+    is_thread = not (len(div_siblings) < 2)
 
-    return pure_text
+    if is_thread:
+        for div in div_siblings[:-1]:
+            pure_text += '{}\n'.format(div.text)
+        return pure_text
+
+    return div_siblings[0].text
 
 if __name__ == '__main__':
     # pure_text = mail_thread_to_text(sample_mail_bodies[-2].get('message_body'))
