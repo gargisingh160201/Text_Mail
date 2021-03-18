db3bb48154d87faa5a5aa9fc5083c0cf7eee9169
Deepam Gupta <deepam.deepam.gupta@gmail.com>
2021-03-18 19:10:08 +0530

    midway: mailspring ke liye

--- main.py
+++ main.py
@@ -1,40 +1,59 @@
-from sample_test_cases import hotmail
+from sample_test_cases import hotmail_outlook, gmail_mailspring
 from bs4 import BeautifulSoup
 
-def mail_thread_to_text(mail_txt: str):
+def trim_the_mail(mail_content: str, mail_client: str):
 
-    if not mail_txt:
-        return mail_txt
+    # mail_content
+    if not mail_content:
+        return mail_content
 
-    soup = BeautifulSoup(mail_txt, 'lxml')
-    is_html = bool(soup.find())
-    if not is_html:
-        return mail_txt
+    soup = BeautifulSoup(mail_content, 'lxml')
 
-    first_div = soup.find(['div', 'p']) # p for thunderbird
-    # div_siblings = [first_div] + (first_div.find_next_siblings('div'))
+    # mail_content not html
+    is_mail_html = bool(soup.find())
+    if not is_mail_html:
+        return mail_content
 
-    # pure_text = ''
-    #
-    # is_thread = not (len(div_siblings) < 2)
-    #
-    # if is_thread:
-    #     for div in div_siblings[:-1]:
-    #         pure_text += '{}\n'.format(div.text)
-    #     return pure_text
-    #
-    # return div_siblings[0].text
+    if mail_client == 'gmail':
+        return trim_mailspring(soup, mail_client)
+    elif mail_client == 'mailspring':
+        return trim_mailspring(soup, mail_client)
 
-    return first_div.text
+    return mail_content
+
+def trim_mailspring(soup, mail_client: str):
+    root_elements = soup.body.findChildren()
+
+    gmail_quote_attribution_index = -1
+    for i, element in enumerate(root_elements):
+        if element.attrs.get('class') == 'gmail_quote_attribution':
+            gmail_quote_attribution_index = i
+            break
+    else:
+        return soup.get_text(separator='\n', strip=True)
+
+    relevant_elements = root_elements[:gmail_quote_attribution_index]
+
+    pure_text = ""
+
+    for element in relevant_elements:
+        element += element.text or ''
+
+    return pure_text
+
+
+def trim_gmail(soup, mail_client: str):
+    pass
+
 
 if __name__ == '__main__':
-    # pure_text = mail_thread_to_text(sample_mail_bodies[-2].get('message_body'))
-    # print(pure_text)
 
     all_mail_text = []
+    mail_client = 'mailspring'
 
-    sample_mails = [mail['message_body'] for mail in hotmail.mail_responses]
-    for mail in sample_mails:
-        all_mail_text.append(mail_thread_to_text(mail))
+    sample_mails_list = [mail_response['message_body'] for mail_response in gmail_mailspring.mail_responses]
+
+    for mail in sample_mails_list:
+        all_mail_text.append(trim_the_mail(mail, mail_client))
 
     print(all_mail_text)
