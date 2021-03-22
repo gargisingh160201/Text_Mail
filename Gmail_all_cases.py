04a127294adf5327184a5c161d79011d189d65af
Gargi Singh <gargisingh297@gmail.com>
2021-03-22 19:34:40 +0530

    HTML return

--- Gmail_all_cases.py
+++ Gmail_all_cases.py
@@ -1,15 +1,24 @@
 from sample_test_cases import Gmail
 from bs4 import BeautifulSoup
 from sample_test_cases import Gmail
-def mail_thread_to_text(mail_txt: str):
+import json
+def trim_the_mail(mail_text: str, mail_client: str):
 
-    if not mail_txt:
-        return mail_txt
 
-    soup = BeautifulSoup(mail_txt, 'lxml')
+
+    soup = BeautifulSoup(mail_text, 'lxml')
     is_html = bool(soup.find())
-    if not is_html:
-        return mail_txt
+    if not is_html: # exception
+        return mail_text
+    if mail_client == 'outlook':
+        return trim_outlook(soup, mail_client)
+    elif mail_client == 'gmail':
+        return trim_gmail(soup)
+
+    return mail_text
+def trim_gmail(soup):
+
+
 
     first_div = soup.find('div',{"dir": "ltr"})
 
@@ -19,27 +28,19 @@ def mail_thread_to_text(mail_txt: str):
 
     # div_siblings = [first_div] + (first_div.find_next_siblings('div'))
 
-    # pure_text = ''
-    #
-    # is_thread = not (len(div_siblings) < 2)
-    #
-    # if is_thread:
-    #     for div in div_siblings[:-1]:
-    #         pure_text += '{}\n'.format(div.text)
-    #     return pure_text
-    #
-    # return div_siblings[0].text
 
-    return first_div.get_text(separator="\n",strip=True)
+
+    return str(first_div)
 
 if __name__ == '__main__':
-    # pure_text = mail_thread_to_text(sample_mail_bodies[-2].get('message_body'))
-    # print(pure_text)
-
     all_mail_text = []
+    mail_client = 'gmail'
 
-    sample_mails = [mail['message_body'] for mail in Gmail.Responses]
-    for mail in sample_mails:
-        all_mail_text.append(mail_thread_to_text(mail))
+    sample_mails_list = [response['message_body'] for response in Gmail.Responses]
 
-    print(all_mail_text)
+    for sno, mail in enumerate(sample_mails_list):
+        trimmed_mail = trim_the_mail(mail, mail_client)
+        all_mail_text.append({"sno": sno, "message_body": trimmed_mail})
+
+    with open('output.json', 'w') as f:
+        json.dump(all_mail_text, f)
