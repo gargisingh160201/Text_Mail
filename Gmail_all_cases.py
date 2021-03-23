5f751a0b19dc82cb1ffcfca118676bfad6e70fd5
Gargi Singh <gargisingh297@gmail.com>
2021-03-23 19:02:06 +0530

    Covered Test cases and added logger

--- Gmail_all_cases.py
+++ Gmail_all_cases.py
@@ -2,43 +2,51 @@ from sample_test_cases import Gmail
 from bs4 import BeautifulSoup
 from sample_test_cases import Gmail
 import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+
 def trim_the_mail(mail_text: str, mail_client: str):
-
-
-
     soup = BeautifulSoup(mail_text, 'lxml')
     is_html = bool(soup.find())
-    if not is_html: # exception
+    if not is_html:
         return mail_text
     if mail_client == 'gmail':
         return trim_gmail(soup)
 
     return mail_text
+
+
 def trim_gmail(soup):
+    try:
+        first_div = soup.find('div', {"dir": "ltr"})
 
-
-
-    first_div = soup.find('div',{"dir": "ltr"})
-
-    for div in soup.find_all("div", {'class': 'gmail_signature'}):
-        div.decompose()
-
+        for div in soup.find_all("div", {'class': 'gmail_signature'}):
+             div.decompose()
 
     # div_siblings = [first_div] + (first_div.find_next_siblings('div'))
 
+        return str(first_div)
+    except Exception as e:
+        logger.exception(e)
+        print(e)
 
 
-    return str(first_div)
 
 if __name__ == '__main__':
-    all_mail_text = []
-    mail_client = 'gmail'
+    try:
+        all_mail_text = []
+        mail_client = 'gmail'
 
-    sample_mails_list = [response['message_body'] for response in Gmail.Responses]
+        sample_mails_list = [response['message_body'] for response in Gmail.Responses]
 
-    for sno, mail in enumerate(sample_mails_list):
-        trimmed_mail = trim_the_mail(mail, mail_client)
-        all_mail_text.append({"sno": sno, "message_body": trimmed_mail})
+        for sno, mail in enumerate(sample_mails_list):
+            trimmed_mail = trim_the_mail(mail, mail_client)
+            all_mail_text.append({"sno": sno, "message_body": trimmed_mail})
 
-    with open('output.json', 'w') as f:
-        json.dump(all_mail_text, f)
+        with open('output.json', 'w') as f:
+            json.dump(all_mail_text, f)
+    except FileNotFoundError as e:
+        logger.exception('Error while dumping file')
+        print(e)
